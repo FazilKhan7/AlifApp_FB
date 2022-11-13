4e81e678988a0de87f93636ff14cde95a9ae555f
Nurdaulet <bagdauletovnurdaulet03@gmail.com>
2022-11-13 10:48:48 +0600

    Initial Commit

---
+++ PrayTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrayTime
+//
+//  Created by Nurdaulet on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
